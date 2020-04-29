5f485e546fe990cf866357d73878a890f956f6d4
GlinkaG <grzegorzg199@gmail.com>
2020-04-29 21:49:12 +0200

    GG 2020-04-29

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,49 +1,45 @@
 #import bibliotek
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 
-#wyswietlanie zawartosci
+#wyświetlenie zawartości katalogu opinions
 print(os.listdir("./app/opinions"))
 
-#
+#wczytanie id produktu, którego opinie będą analizowane
 product_id = input("Podaj kod produktu: ")
-
 
 opinions = pd.read_json("app/opinions/"+product_id+".json")
 opinions = opinions.set_index("opinion_id")
 
-opinions['stars'] = opinions['stars'].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
 
-#histogram częstości występowania poszczególnych ocen
-stars = opinions['stars'].value_counts().sort_index().reindex(list(np.arange(0, 5.5, 0.5)), fill_value=0)
-
+#histogram częstości występowania poszczególnych ocen 
+stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
 fig, ax = plt.subplots()
 stars.plot.bar(color="lightskyblue")
 ax.set_title("Gwiazdki")
-ax.set_xlabel("Liczba gwiazdek")
-ax.set_ylabel("Liczba opinii")
-plt.savefig("app/figures/"+product_id+"_bar.png")
+ax.set_xlabel("liczba gwiazdek")
+ax.set_ylabel("liczba opinii")
+plt.savefig("app/static/figures/"+product_id+"_bar.png")
 plt.close()
-
 
 #udział poszczególnych rekomendacji w ogólnej liczbie opinii
-
-recommendation = opinions['recommendation'].value_counts()
+recommendation = opinions["recommendation"].value_counts()
 fig, ax = plt.subplots()
-recommendation.plot.pie(label="", autopct="%1.1f%%")
-plt.savefig("figures/"+product_id+"_pie.png")
+recommendation.plot.pie(label="", autopct="%1.1f%%", colors=['forestgreen', 'crimson'])
+ax.set_title("Rekomendacje")
+plt.savefig("app/static/figures/"+product_id+"_pie.png")
 plt.close()
 
-average_score = opinions['stars'].mean()
-pros = opinions['pros'].count()
-cons = opinions['cons'].count()
-purchased = opinions['purchased'].sum()
+average_score = opinions["stars"].mean()
+pros = opinions["pros"].count()
+cons = opinions["cons"].count()
+purchased = opinions["purchased"].sum()
 
 print(average_score, pros, cons, purchased)
 
-stars_purchased = pd.crosstab(opinions['stars'], opinions['purchased'])
+stars_purchased = pd.crosstab(opinions["stars"], opinions["purchased"])
+
 print(stars_purchased)
-
-
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,33 @@
 from app import app
 from flask import render_template
+from flaskext.markdown import Markdown
+from app.forms import ProductForm
+app.config['SECRET-KEY'] = "haselo"
+
+Markdown(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
 
-@app.route('/scraper')
-def scraper():
-    return "Podaj kod produktu do pobrania opinii"
+@app.route('/extract')
+def extract():
+    if request.method == "POST":
+        return "Przesłano formularz"
+    form = ProductForm()
+    return render_template("extract.html", text=form)
+
+
+@app.route('/products')
+def products():
+    pass
+
+@app.route('/about')
+def about():
+    with open("README.md", "r", encoding="UTF-8") as f:
+        content = f.read()
+    return render_template("about.html", text=content)
 
 @app.route('/analyzer/<product_id>')
 def analyzer():
