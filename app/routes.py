d3a011a581f281bdf0302089c66b07d446073cbc
GlinkaG <grzegorzg199@gmail.com>
2020-05-13 15:42:17 +0200

    GG 2020-05-13

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,9 @@ from app import app
 from flask import render_template, request
 from flaskext.markdown import Markdown
 from app.forms import ProductForm
-app.config['SECRET_KEY'] = "Tajemniczy_mysi_sprzęt"
+from app.models import Product, Opinion
+import requests
+app.config['SECRET_KEY'] = "haselohaselo"
 
 Markdown(app)
 
@@ -22,7 +24,14 @@ def about():
 def extract():
     form = ProductForm()
     if form.validate_on_submit():
-        return "Przesłano formuarz"
+        page_response = requests.get("https://www.ceneo.pl/"+request.form['product_code'])
+        if  page_response.status_code == 200:
+            product = Product(request.form['product_code'])
+            product.extract_product()
+            return "OK"
+        else:
+            form.product_code.errors.append("Dla podanego kodu nie ma produktu")
+            return render_template("extract.html", form=form)
     return render_template("extract.html", form=form)
 
 @app.route('/products')
@@ -33,7 +42,6 @@ def products():
 def product():
     pass
 
-
 @app.route('/analyzer/<product_id>')
 def analyzer():
-    pass
+    return "Podaj kod produktu do analizy"
